7ec7bd9346cc2adf171dfc6f1bec96435224da0c
GBardis <crisolivasavatage@hotmail.com>
2017-08-05 13:40:13 +0300

    firebase hosting and database integration

--- app/controllers/_contact.js
+++ app/controllers/_contact.js
@@ -13,12 +13,5 @@ export default Ember.Controller.extend({
 	isDisabled: Ember.computed.not('isLongEnough'),
 
 
-	actions: {
-		saveInvitation() {
-			alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
-			this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
-			this.set('emailAddress', '');
-			this.set('message', '');
-		}
-	}
+
 });
--- app/models/contact.js
+++ app/models/contact.js
@@ -2,5 +2,23 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
 	emailAddress: DS.attr('string'),
-	message: DS.attr('string')
+	message: DS.attr('string'),
+
+	// isValid: function() {
+	// 	let email = this.get('emailAddress');
+	// 	if (email.match(email, /^.+@.+\..+$/)) {
+	// 		return true;
+	// 	} else {
+	// 		return false;
+	// 	}
+	// }.property('emailAddress'),
+
+	actions: {
+		saveInvitation() {
+			//alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
+			this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+			this.set('emailAddress', '');
+			this.set('message', '');
+		}
+	}
 });
